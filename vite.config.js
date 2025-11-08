1126748c42877d6d39fbd743ef0c1ebdab61955e
Francisco Bennassar <franbenna@gmail.com>
2025-11-08 00:42:23 -0400

    move to php

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  // CORRECCIÓN CRUCIAL: Esto fuerza a Vite a usar rutas relativas 
-  // (ej. './index.js') en lugar de rutas absolutas (ej. '/index.js').
-  base: './', 
-});
